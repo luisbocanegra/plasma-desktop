2a54431557ce2fc249e175a7e0d5f4f6d1e7983c
Eike Hein <hein@kde.org>
2016-06-06 16:49:19 +0900

    Import Task Manager widgets ports to the new library.
    Summary: Depends on D1721, D1723.
    
    Reviewers: #plasma
    
    Subscribers: plasma-devel
    
    Tags: #plasma
    
    Differential Revision: https://phabricator.kde.org/D1724

--- kcms/keyboard/config-keyboard.h.cmake
+++
@@ -1,9 +0,0 @@
-#ifndef CONFIG_KEYBOARD_H
-#define CONFIG_KEYBOARD_H
-
-#cmakedefine HAVE_XINPUT
-#cmakedefine HAVE_UDEV
-
-#cmakedefine NEW_GEOMETRY
-
-#endif // CONFIG_KEYBOARD_H
--- kcms/translations/Messages.sh
+++
@@ -1,4 +0,0 @@
-#! /usr/bin/env bash
-$EXTRACTRC *.ui >> rc.cpp
-$XGETTEXT -ktranslate:1,1t -ktranslate:1c,2,2t *.cpp -o $podir/kcmtranslations.pot
-rm -f rc.cpp
