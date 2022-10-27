ca228f6af6227b973e0ba185d7e0643cb4c0a99a
Alexandra <alexankitty@gmail.com>
2022-10-27 03:53:08 -0600

    Create installer

--- install.sh
+++ install.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-kpackagetool5 -i $SCRIPT_DIR
+kpackagetool5 -i .
 cp ./FancyTasks.png ~/.local/share/icons/hicolor/256x256/apps/FancyTasks.png
